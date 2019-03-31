df4e53c2f32cb6862a56c494d7f22a805357913a
minnecoder <sdparkin@gmail.com>
2019-03-31 12:44:49 -0500

    some minor changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,17 @@
-module.exports = {
-    "parser": "babel-eslint",
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "settings": {
-        "ecmascript": 6,
-        "jsx": true
-    },
-    "extends": "airbnb",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
+{
+    "extends": [
+      "wesbos"
     ],
     "rules": {
-        "react/jsx-filename-extension": 0,
+      "no-console": 2,
+      "prettier/prettier": [
+        "error",
+        {
+          "trailingComma": "es5",
+          "singleQuote": true,
+          "printWidth": 120,
+          "tabWidth": 8,
+        }
+      ]
     }
-};
+  }
